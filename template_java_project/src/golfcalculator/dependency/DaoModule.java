4eb40ff6850404d65f51d4c5c90c7b929903e99c
Alexander Rodriguez-Zingg <alexrodriguezzingg@icloud.com>
2024-04-09 21:26:35 -0600

    Set up Dagger and implemented CreateUserActivityProvider

--- template_java_project/src/golfcalculator/dependency/DaoModule.java
+++ template_java_project/src/golfcalculator/dependency/DaoModule.java
@@ -12,12 +12,17 @@ import javax.inject.Singleton;
 @Module
 public class DaoModule {
 
-    private AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
-            .withRegion("us-west-2")
-            .build();
+    @Provides
+    @Singleton
+    public AmazonDynamoDB provideAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withRegion("us-west-2")
+                .build();
+    }
+
     @Provides
     @Singleton
     public DynamoDBMapper provideDynamoDBMapper() {
-        return new DynamoDBMapper(amazonDynamoDB);
+        return new DynamoDBMapper(provideAmazonDynamoDB());
     }
 }
--- template_java_project/src/golfcalculator/lambda/CreateUserActivityProvider.java
+++ template_java_project/src/golfcalculator/lambda/CreateUserActivityProvider.java
@@ -3,6 +3,7 @@ package golfcalculator.lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import dagger.internal.DaggerCollections;
+import golfcalculator.dependency.DaggerServiceComponent;
 import golfcalculator.dependency.ServiceComponent;
 import golfcalculator.models.requests.CreateUserRequest;
 import golfcalculator.models.results.CreateUserResult;
@@ -15,16 +16,14 @@ public class CreateUserActivityProvider implements RequestHandler<CreateUserRequ
     }
 
     @Override
-    public CreateUserResult handleRequest(CreateUserRequest input, Context context) {
-        return null;
+    public CreateUserResult handleRequest(CreateUserRequest createUserRequest, Context context) {
+        return getServiceComponent().provideCreateUserActivity().handleRequest(createUserRequest, context);
     }
 
     private ServiceComponent getServiceComponent() {
-
         if (serviceComponent == null) {
-            serviceComponent = null;
+            serviceComponent = DaggerServiceComponent.create();
         }
-
-        return null;
+        return serviceComponent;
     }
 }
